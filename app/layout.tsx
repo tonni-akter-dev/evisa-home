ee97252f4f21eb307ed7715bf04912dd2bfa24c3
Tonni Akter <85512465+tonni-akter-dev@users.noreply.github.com>
2026-04-21 14:57:49 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 // src/app/layout.tsx
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 // import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Home',
   description: 'Your app description',
 }
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  userScalable: true,
+};
 
 export default function RootLayout({
   children,
